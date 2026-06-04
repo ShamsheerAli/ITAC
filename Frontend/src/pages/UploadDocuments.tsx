7c60abf6d98e87993b2b7e15812759da7e4f9b49
Shamsheer Ali <80998199+ShamsheerAli@users.noreply.github.com>
2026-06-04 21:20:59 +0000

    New file added in upload docs page

--- Frontend/src/pages/UploadDocuments.tsx
+++ Frontend/src/pages/UploadDocuments.tsx
@@ -98,6 +98,7 @@ const UploadDocuments = () => {
           <DownloadRow label="Confidentiality Statement" fileName="confidentiality_statement.pdf" />
           <DownloadRow label="Media Release form" fileName="media_release.pdf" />
           <DownloadRow label="Energy Assessment Application" fileName="energy_application.pdf" />
+          <DownloadRow label="PSO Consent" fileName="pso_consent.pdf" />
         </div>
 
         {/* ======================= */}
